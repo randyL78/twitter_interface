5d299abac967097f9886c3bf38428249605c3a69
RandyL <30959856+randyL78@users.noreply.github.com>
2018-01-06 18:56:56 -0500

    install initial dependencies

---
+++ app.js
@@ -0,0 +1,15 @@
+// Dependent libriaries
+const express = require('express');
+
+// Global variables
+const app = express();
+const port = 3011;
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Initial project started</h1>`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
