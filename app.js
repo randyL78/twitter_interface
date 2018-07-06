b00f7472e470f764bd4b6a33d28587c2be9ebf1b
Randy's Mac <randydavidl78@gmail.com>
2018-07-05 23:15:04 -0400

    Add static pug head, header, and footer

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 // Dependent libriaries
 const express = require('express');
+const path = require('path');
 
-/* Custom middleware imports */
+// Custom middleware imports 
 const routes = require("./middleware/routes.js")
 
 
 // Global variables
 const app = express();
-const port = process.env.PORT || 3030;
+const port = 3040;
 
 /* Set Express to render pug/jade */
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+/* Set location to look for non JS assets, primarily css */
+app.use(express.static(path.join(__dirname, 'public')));
 
 /* have route file handle all routing */
 app.use('/', routes);
-
-
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
--- middleware/routes.js
+++ middleware/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // GET home route
 router.get('/', (req, res, next)  => {
-  res.send(`<h1>Initial project started</h1>`);
+  res.render('index');
 })
 
 module.exports = router;
