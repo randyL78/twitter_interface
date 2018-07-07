22a97677a5ce7455879bd80d7ac101048b765e0b
Randy's Mac <randydavidl78@gmail.com>
2018-07-07 19:38:07 -0400

    Remove createData.js

--- app.js
+++ app.js
@@ -4,18 +4,10 @@ const path = require('path');
 
 // Custom middleware imports 
 const routes = require("./middleware/routes");
-const createData = require("./middleware/createData");
-const twitData = require('./middleware/twitData');
 
 // Global variables
 const app = express();
 const port = 3040;
-
-/* Create static data for now to pass to Pug */
-app.use(createData);
-
-/* get working on retrieving twitter data */
-// app.use(twitData);
 
 /* Set Express to render pug/jade */
 app.set('views', path.join(__dirname, 'views'));
