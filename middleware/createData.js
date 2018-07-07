513240d19a6bc7e91418de40b7db3bccd40596cc
Randy's Mac <randydavidl78@gmail.com>
2018-07-06 20:02:18 -0400

    Add Direct messages dynamically

--- middleware/createData.js
+++ middleware/createData.js
@@ -9,8 +9,73 @@ const createData = (req, res, next) => {
         time      : "4h",
         retweets  : 34,
         likes     : 87
-      }
-    ]
+      },
+      {
+        name      : "Mark Spore",
+        username  : "@mkspore",
+        message   : "My wife is crazy if she thinks a pretzel is a chip.",
+        time      : "3h",
+        retweets  : 4,
+        likes     : 132
+      },
+    ],
+    following : [
+      {
+        name    : "Mark Spore",
+        username: "@mkspore",
+        followed: true
+      },
+      {
+        name    : "Jill Spore",
+        username: "@jillspore",
+        followed: true
+      },
+      {
+        name    : "Narwell Seaford",
+        username: "@oceanswimming",
+        followed: false
+      },
+      {
+        name    : "Sandy Fisher",
+        username: "@sandyfisher",
+        followed: true
+      },
+      {
+        name    : "Lexi Layne",
+        username: "@lexiloulayne",
+        followed: true
+      },
+    ],
+    directMessage : { 
+      name : "Sandy Fisher",
+      messages : [
+        {
+          message : "How are things",
+          time    : 3,
+          them    : true
+        },
+        {
+          message : "They're good!",
+          time    : 3,
+          them    : false
+        },
+        {
+          message : "Glad to hear it",
+          time    : 2,
+          them    : true
+        },
+        {
+          message : "Thanks",
+          time    : 2,
+          them    : false
+        },
+        {
+          message : "You're welcome!",
+          time    : 1,
+          them    : true
+        },        
+      ]
+    }
   };
   req.data = data
   next();
